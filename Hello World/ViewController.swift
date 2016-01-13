7096a6cc3fdfe202bc8a52f6036a53b41d3b1f39
IMAC DESARROLLO 01 <usrimacdes01@pruebasc4.local>
2016-01-13 12:49:58 -0600

    cambio 1

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //modif 1
+    @IBOutlet var miEtiqueta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ejecutaMetodo(sender: AnyObject) {
+    
+        miEtiqueta.text = "Hola mundo"
+    }
 
 }
 
